81dd39b0d4ffd08d70cc1af24bc54cac1ef8a44e
Shelia Teo <teo.zihui.shelia@gmail.com>
2024-05-16 23:07:13 +0800

    created form builder and navbar

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import FormBuilder from "./FormBuilder";
+import { NavigationBar } from "../components/NavigationBar";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App() {
+  return (
+    <div className="flex min-h-screen">
+      <NavigationBar />
+      <div className="flex-grow">
+        <FormBuilder />
+      </div>
+    </div>
+  );
 }
+
