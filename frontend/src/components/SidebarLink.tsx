041683ed4b785a69484637755ebd252725e2418a
Shelia Teo <teo.zihui.shelia@gmail.com>
2024-05-17 10:05:40 +0800

    tidied up sidebar code

---
+++ frontend/src/components/SidebarLink.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface SidebarLinkProps {
+    href: string;
+    icon: React.ReactNode;
+    text: string;
+  }
+  
+  const SidebarLink: React.FC<SidebarLinkProps> = ({ href, icon, text }) => {
+    return (
+    <li>
+      <a
+        href={href}
+        className="flex items-center rounded-lg px-3 py-2 text-slate-900 hover:bg-slate-100 dark:text-white dark:hover:bg-slate-700"
+      >
+        {icon}
+        <span className="ml-3 flex-1 whitespace-nowrap">{text}</span>
+      </a>
+    </li>
+  );
+}
+
+export default SidebarLink;
