11acf5e45e8cc219535362a30b035a0b7a667007
Jun Yu <chuajunyu1@gmail.com>
2024-05-17 16:37:41 +0800

    add auth pages layout

--- frontend/app/form/page.tsx
+++ frontend/app/form/page.tsx
@@ -1,7 +1,7 @@
 import "./FormBuilder.css";
 import type { AppProps } from "next/app";
-import FormBuilder from "../../components/FormBuilder";
-import { NavigationBar } from "../../components/NavigationBar";
+import FormBuilder from "@/components/FormBuilder";
+import { NavigationBar } from "@/components/NavigationBar";
 
 export default function App() {
   return (
