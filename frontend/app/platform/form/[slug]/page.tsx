29a2a375fb34cc45c67c8d7e5bf635f3c003c319
Shelia Teo <teo.zihui.shelia@gmail.com>
2024-05-30 15:24:23 +0800

    added functionality to submit response button

--- frontend/app/platform/form/[slug]/page.tsx
+++ frontend/app/platform/form/[slug]/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import ResponseForm from "@/components/ResponseForm";
 import readFormData from "@/database/read_form";
 
@@ -6,10 +5,11 @@ import readFormData from "@/database/read_form";
 export default await async function Page({ params }: { params: { slug: string } }) {
     const formData = await readFormData(params.slug);
     console.log(formData);
+    const { createdDate, creatorId, description, questions, title } = formData;
+    console.log(questions);
     return <div>
-        <div className="flex">
-            <h1>Hello this is form id {params.slug}</h1>
-            <ResponseForm title="jy being mean" description="hehe" question={[{ id: 1, text: "grr" }, { id: 2, text: "" }]} />
+        <div className="flex flex-col">
+            <ResponseForm title={title} description={description} questions={questions} formId={params.slug}/>
         </div>
     </div>
 }
