048277b16821caeffe249468c313503267daaa35
Jun Yu <chuajunyu1@gmail.com>
2024-05-31 13:58:10 +0800

    Add ProtectedRoute/RedirectIfLoggedIn components where needed

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -11,27 +11,27 @@ export default function Dashboard() {
 
     return (
         <RedirectIfLoggedIn>
-        <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
-                <h1 className="text-3xl font-bold">Login</h1>
-                <p className="text-balance text-muted-foreground">
-                    Select one of the following log-in methods
-                </p>
+            <div className="mx-auto grid w-[350px] gap-6">
+                <div className="grid gap-2 text-center">
+                    <h1 className="text-3xl font-bold">Login</h1>
+                    <p className="text-balance text-muted-foreground">
+                        Select one of the following log-in methods
+                    </p>
+                </div>
+                <EmailLogIn />
+                <div className="grid gap-2">
+                    <SignInGoogle />
+                </div>
+                {/* <div className="grid gap-2">
+                    <SignInGithub />
+                </div> */}
+                <div className="mt-4 text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <Link href="signup" className="underline">
+                        Sign up
+                    </Link>
+                </div>
             </div>
-            <EmailLogIn />
-            <div className="grid gap-2">
-                <SignInGoogle />
-            </div>
-            {/* <div className="grid gap-2">
-                <SignInGithub />
-            </div> */}
-            <div className="mt-4 text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <Link href="signup" className="underline">
-                    Sign up
-                </Link>
-            </div>
-        </div>
         </RedirectIfLoggedIn>
     );
 }
