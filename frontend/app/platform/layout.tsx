46f22fd5c1d402f5745e96ada6e8754b776c94d1
Jun Yu <chuajunyu1@gmail.com>
2024-05-31 16:14:00 +0800

    update method to get user email via firebase auth methods instead

--- frontend/app/platform/layout.tsx
+++ frontend/app/platform/layout.tsx
@@ -1,15 +1,22 @@
+"use client"
+
 import "@/app/globals.css";
 import { NavigationBar } from "@/components/NavigationBar";
+import auth from "@/lib/firebase/app";
+import { useAuth } from "@/lib/firebase/AuthContext";
 
-export default async function RootLayout({
+export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const { user, loading } = useAuth();
+    const email = user?.email ?? "";
+
     return (
         <div className="flex flex-row">
             <div>
-                <NavigationBar user={"test"} />
+                <NavigationBar user={email} />
             </div>
             <div className="flex-auto">{children}</div>
         </div>
