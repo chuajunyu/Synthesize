a73bbdf86c47bdf8219198b5507c48e772b47387
Jun Yu <chuajunyu1@gmail.com>
2024-06-30 00:16:38 +0800

    Add todo for preventing excessive api calls to openai

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+
 from fastapi import FastAPI, Response, status
+
 from services.OpenAiService import OpenAiService
 from services.FirebaseService import FirebaseService
 
@@ -9,16 +12,18 @@ app = FastAPI()
 open_ai_service = OpenAiService()
 firebase_service = FirebaseService()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World", "time": str(datetime.now())}
 
-@app.get("/get_form_analysis/{formId}", status_code=200)
+@app.get("/get_form_analysis/{formId}", status_code=status.HTTP_200_OK)
 async def get_form_analysis(formId: str, response: Response, secret: str | None = None):
     if secret != os.environ['SECRET_KEY']:
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return {"message": "Invalid secret key"}
+    
+    # TODO: Should check FIREBASE to see when this analysis was last updated and return that instead of re-analysing
+    
     formatted_responses = firebase_service.get_formatted_responses(formId)
     business_context = firebase_service.get_form_description(formId)
     analysis = open_ai_service.analyse_responses(business_context, formatted_responses)
