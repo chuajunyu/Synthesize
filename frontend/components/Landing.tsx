5c273e8c72d69b28ad8460612e91b212885dc059
Jun Yu <chuajunyu1@gmail.com>
2024-07-06 23:11:58 +0800

    Add Mobile & Desktop Responsiveness to Landing Page

--- frontend/components/Landing.tsx
+++ frontend/components/Landing.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function LandingPage() {
+export default function Landing() {
     return (
-        <div className="flex flex-col h-screen w-full transform bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-            <div className="flex flex-row py-5 px-12 justify-between items-center">
+        <div className="flex flex-col min-h-screen w-full transform bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 2xl:px-80">
+            <div className="flex flex-row py-5 px-6 sm:px-12 justify-between items-center">
                 <div>
                     <h2 className="text-2xl text-center font-medium text-white">
                         Synthesize
@@ -18,19 +18,19 @@ export default function LandingPage() {
                     </Link>
                 </div>
             </div>
-            <div className="py-5 px-20 flex flex-row mt-5">
-                <div className="flex flex-col w-1/2">
-                    <h1 className="text-7xl text-wrap text-white mb-5 font-bold">
+            <div className="flex flex-col md:flex-row py-5 px-4 sm:px-20 mt-5">
+                <div className="flex flex-col sm:w-1/2">
+                    <h1 className="text-5xl text-center sm:text-7xl sm:text-left text-wrap text-white mb-5 font-bold">
                         Transform your feedback into insights
                     </h1>
-                    <p className="text-white text-2xl mb-5">
+                    <p className="text-white text-2xl text-center sm:text-left mb-5">
                         Aggregate and analyze customer sentiments quickly using
                         Synthesize and get actionable insights for your
                         businesses or projects fast.
                     </p>
-                    <div className="">
+                    <div className="flex flex-row justify-center sm:justify-start">
                         <Link href="/auth/signup">
-                            <button className="rounded-full px-5 py-3 text-2xl text-white bg-slate-300/20 font-semibold shadow-lg flex flex-row justify-center items-center">
+                            <button className="flex flex-row items-center rounded-full px-5 py-3 self-center text-lg sm:text-2xl text-white bg-slate-300/20 font-semibold shadow-lg ">
                                 Get started with Synthesize now
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -50,22 +50,22 @@ export default function LandingPage() {
                         </Link>
                     </div>
                 </div>
-                <div className="w-1/2">
-                    <Image 
+                <div className="flex flex-col mb-10 sm:mb-0 sm:w-1/2 sm:justify-center ">
+                    <Image
                         src="/truffle.png"
                         alt="landing"
                         width={500}
                         height={500}
-                        className="mt-10 ml-16 rounded-2xl shadow-xl"
+                        className="mt-10 sm:ml-16 rounded-2xl shadow-xl"
                     />
-                    <p className="ml-16 mt-3 text-white italic text-right">
-                        I discovered what my customers thought of McDonald's new Truffle Cheese Sauce in just 5 minutes using Synthesize!
+                    <p className="sm:ml-16 mt-3 text-white italic text-center sm:text-right">
+                        I discovered what my customers thought of McDonald's new
+                        Truffle Cheese Sauce in just 5 minutes using Synthesize!
                     </p>
-                    <p className="ml-16 text-white italic font-light text-right">
+                    <p className="ml-16 text-white italic font-light text-center sm:text-right">
                         - McDonald's Top Employee of the Month
                     </p>
                 </div>
-
             </div>
         </div>
     );
